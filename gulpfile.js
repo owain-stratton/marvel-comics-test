514ec82dad60f4a2ab1607d12f468d6e30a32efe
owain-stratton <owain@geoshepherds.com>
2016-08-11 17:59:14 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    maps        = require('gulp-sourcemaps'),
+    browserify  = require('browserify'),
+    source      = require('vinyl-source-stream');
+
+gulp.task('browserify', function() {
+    return browserify('js/app.js').bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('js'));
+});
+
+gulp.task('compileSass', function() {
+  return gulp.src("scss/application.scss")
+      .pipe(maps.init())
+      .pipe(sass())
+      .pipe(maps.write('./'))
+      .pipe(gulp.dest('css'));
+});
+
+gulp.task('watchFiles', function() {
+  gulp.watch('scss/*.scss', ['compileSass']);
+  gulp.watch('js/app.js', ['browserify']);
+});
+
+gulp.task('serve', ['watchFiles']);
---
+++ js/app.js
@@ -0,0 +1,100 @@
+var $ = require('jquery'),
+    backbone = require('backbone'),
+    _ = require('underscore'),
+    CryptoJS = require('crypto-js');
+
+var publicKey = '241328638ad6321eb015fa420ebb26ec',
+    privateKey = 'ef49e3bbefc86b92aad3aa192719a4f50d9a4f92',
+    ts = new Date().getTime(),
+    hash = CryptoJS.MD5(ts + privateKey + publicKey).toString(),
+    characterID = '1009718'; //Wolverine
+
+var charModel = backbone.Model.extend({});
+
+var charCollection = backbone.Collection.extend({
+  model: charModel,
+  url: 'http://gateway.marvel.com:80/v1/public/characters/' + characterID + '?apikey=' + publicKey
+});
+
+var character = new charCollection();
+
+//GET request to Marvel API for Wolverine character
+character.fetch({
+  ts: ts,
+  hash: hash,
+  success: function(data_array) {
+    var data = data_array.models[0].attributes;
+    getCharInfo(data);
+  }
+});
+
+function createCharObject(charImg, charName, charDesc, attrText) {
+  this.charImg = charImg;
+  this.charName = charName;
+  this.charDesc = charDesc;
+  this.attrText = attrText;
+}
+
+var getCharInfo = function(data){
+  var results = data.data.results[0];
+  var characterObject = new createCharObject(makeThumbnailPath(results), results.name, results.description, data.attributionHTML);
+  
+  var html = '<div class="attrText">' + characterObject.attrText + '</div>';
+      html += '<div class="characterWrapper">';
+      html += '<img class="charImg" src=' + characterObject.charImg + ' alt=' + characterObject.charName + '/>';
+      html += '<h1>' + characterObject.charName + '</h1>';
+      html += '<h4>' + characterObject.charDesc + '</h4>';
+      html += '</div>';
+      html += '<h4>Comics List:</h4>';
+  
+  printHTML(html); 
+  getComicCollection(results);
+}
+
+function getComicCollection(results) {
+  var comicURI = results.comics.collectionURI;
+  var comicModel = backbone.Model.extend({});
+  
+  var comicCollection = backbone.Collection.extend({
+    model: comicModel,
+    url: comicURI + '?apikey=' + publicKey
+  });
+  
+  var comicList = new comicCollection();
+  
+  //GET request to Marvel API for the comics with Wolverine
+  comicList.fetch({
+    ts: ts,
+    hash: hash,
+    success: function(data_array) {
+      var data = data_array.models[0].attributes;
+      getComics(data);
+    }
+  });
+}
+
+function getComics(results) {
+  _.each(results.data.results, function(i) {
+    var comicImgPath = makeThumbnailPath(i);
+  
+    var html = '<div class="comics">'; 
+        html += '<img src=' + comicImgPath + ' alt=' + i.title + '/>';
+        html += '<h2>' + i.title + '</h2>';
+        html += '<p>' + i.description + '</p>';  
+        html += '</div>'
+    
+    printHTML(html);
+  });
+}
+
+function makeThumbnailPath(results) {
+  var path = results.thumbnail.path,
+      ext = results.thumbnail.extension;
+
+  var charImgPath = path + '.' + ext;
+  return charImgPath;
+}
+
+function printHTML(html) {
+  $('#container').append(html);
+}
